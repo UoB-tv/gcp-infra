9c427cbaa2241fbe3bec5c753e8c403f52bcfeba
Zaiyang Li <zl4917@my.bristol.ac.uk>
2018-12-07 14:52:59 +0000

    updated script variables for video-proc-sa

--- terraform/init_video-proc-sa.sh
+++ terraform/init_video-proc-sa.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 
-TF_VAR_project_id=uob-tv-project-dev
-TF_ADMIN=video-proc-sa
+PROJECT_ID=uob-tv-project-dev
+VIDEO_PROC_SA=video-proc-sa
 
 gcloud iam service-accounts create terraform \
   --display-name "Video Processing Service Account"
 
-gcloud projects add-iam-policy-binding ${TF_VAR_project_id} \
-  --member serviceAccount:video-proc-sa@${TF_VAR_project_id}.iam.gserviceaccount.com \
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+  --member serviceAccount:${VIDEO_PROC_SA}@${PROJECT_ID}.iam.gserviceaccount.com \
   --role roles/storage.objectViewer
 
 
-gcloud projects add-iam-policy-binding ${TF_VAR_project_id} \
-  --member serviceAccount:video-proc-sa@${TF_VAR_project_id}.iam.gserviceaccount.com \
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+  --member serviceAccount:${VIDEO_PROC_SA}@${PROJECT_ID}.iam.gserviceaccount.com \
   --role roles/storage.objectCreator
 
 
-gcloud projects add-iam-policy-binding ${TF_VAR_project_id} \
-  --member serviceAccount:video-proc-sa@${TF_VAR_project_id}.iam.gserviceaccount.com \
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+  --member serviceAccount:${VIDEO_PROC_SA}@${PROJECT_ID}.iam.gserviceaccount.com \
   --role roles/pubsub.subscriber
 
 
-gcloud projects add-iam-policy-binding ${TF_VAR_project_id} \
-  --member serviceAccount:video-proc-sa@${TF_VAR_project_id}.iam.gserviceaccount.com \
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+  --member serviceAccount:video-proc-sa@${PROJECT_ID}.iam.gserviceaccount.com \
   --role roles/pubsub.publisher
